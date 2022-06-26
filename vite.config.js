711b317e393712cf5abaeeedf58649cc8487fdac
danielterletzkiy <danielterletzkiy@gmail.com>
2022-06-27 01:17:32 +0200

    HAL-2 vite config cleanup
    Took 2 minutes

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,9 @@ import vue from '@vitejs/plugin-vue';
 var path = require("path");
 var mode = process.env.APP_ENV;
 console.log('APP_ENV:', mode);
-// https://vitejs.dev/config/
 export default defineConfig({
     mode: mode,
     plugins: [vue()],
     resolve: { dedupe: ['vue'] },
-    base: mode === 'production' ? "/".concat(path.resolve(__dirname, "./dist/"), "/") : '',
-    server: {
-        proxy: {
-            '/api': 'http://localhost:2343'
-        }
-    }
+    base: mode === 'production' ? "/".concat(path.resolve(__dirname, "./dist/"), "/") : ''
 });
