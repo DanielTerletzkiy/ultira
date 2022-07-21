f420d171192c6e7d5a5ca421d119eb50c89010cf
danielterletzkiy <danielterletzkiy@gmail.com>
2022-07-22 00:17:41 +0200

    ULT-5 open GitHub project with url
    Took 14 minutes

---
+++ src/components/JiraLinkIconButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <d-icon-button
+      root-tag="a"
+      target="_blank"
+      :href="url"
+      color="primary"
+      style="margin: -6px 0"
+      :size="25"
+  >
+    <d-icon name="external-link-alt" :size="18"/>
+  </d-icon-button>
+</template>
+<script setup lang="ts">
+  defineProps({
+    url: String
+  })
+</script>
+<style scoped lang="scss">
+
+::v-deep(.d-accordion) {
+
+}
+</style>
