d4b3cdd7521adb0b3f6ba1f1a41bc5ed7ac6de32
Daniel Terletzkiy <daniel.terletzkiy@check24.de>
2022-07-25 14:18:44 +0200

    ULT-22 Fixed changing bases

--- express/Server.js
+++ express/Server.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Index_1 = __importDefault(require("./router/Index"));
-const { createProxyMiddleware, fixRequestBody, } = require("http-proxy-middleware");
+const { createProxyMiddleware, fixRequestBody } = require("http-proxy-middleware");
 const ProjectScraper = require("./controller/ProjectScraper");
 const SocketIO = require("./service/SocketIO");
 const http = require("http");
@@ -13,23 +13,27 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-    //delete api hindering headers
-    delete req.headers["user-agent"];
-    next();
-});
 app.use("/api", Index_1.default);
 let targetInstance;
 app.post("/url", function (req, res) {
     targetInstance = req.headers["jira-host"];
     console.log("targetInstance: ", targetInstance);
+    app._router.stack.map((layer, i) => layer.regexp.fast_slash && layer.name === "<anonymous>" ? i : "").filter(String).forEach((i) => {
+        console.log(i);
+        app._router.stack.splice(i, 1);
+    });
     try {
+        app.use((req, res, next) => {
+            //delete api hindering headers
+            delete req.headers["user-agent"];
+            next();
+        });
         app.use("/", createProxyMiddleware({
             target: targetInstance,
             changeOrigin: true,
             protocolRewrite: "https",
             secure: false,
-            onProxyReq: fixRequestBody,
+            onProxyReq: fixRequestBody
         }));
     }
     catch (e) {
